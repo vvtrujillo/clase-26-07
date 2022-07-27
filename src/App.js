61688984446ff32585893e24ef3ea5c4e0fd880c
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-07-26 22:12:56 -0400

    segundo commit clase 26-07

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from './Componente/Navbar';
+import { useState } from 'react';
+import { Button, Col, Container, Row } from 'reactstrap';
+import Swal from 'sweetalert2';
 
-function App() {
+const App = () => {
+
+  const[edad, setEdad] = useState(0);
+  const[arreglo, setArreglo] = useState([1,2,3,4,5,6])
+
+  const aumentar = (e) =>{
+    setEdad(edad+1);
+    //setArreglo
+  }
+
+  const disminuir = (e) =>{    
+
+    if(edad==0){
+      Swal.fire('Edad no puede ser menor a cero','warning');
+    }
+    else{
+      setEdad(edad-1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={12} className='center'><h1>La edad es: {edad}</h1></Col>
+        <Col sm={6} md={3} className='center'><Button color='primary' onClick={aumentar}>Aumentar</Button></Col>
+        <Col sm={6} md={3} className='center'><Button color='danger' onClick={disminuir}>Disminuir</Button></Col>
+      </Row>
+    </Container>
   );
 }
 
